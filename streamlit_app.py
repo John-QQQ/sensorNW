0987a4459dcd5ff5d46fee481adf289812a265fe
John-QQQ <kjk1985@naver.com>
2024-10-24 06:46:50 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈MEMS Sensor Meta-Data 관리!")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Let inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
